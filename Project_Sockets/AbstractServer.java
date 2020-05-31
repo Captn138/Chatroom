0a34151ff3817186a5e0d073cbbe7751684b3093
Mikka138 <mickael.almeida1308@gmail.com>
2020-05-31 14:43:43 +0200

    cleaning

--- Project_Sockets/AbstractServer.java
+++
@@ -1,4 +0,0 @@
-public abstract class AbstractServer
-{
-	public abstract void connect(String ip);
-} 
--- Project_Sockets/ClientConnection.java
+++
@@ -1,8 +0,0 @@
-public class ClientConnection
-{
-	public static void main (String[] args)
-	{
-		SimpleClient c1 = new SimpleClient();
-		c1.connect("localhost");
-	}
-}
