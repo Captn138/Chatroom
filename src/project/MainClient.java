b394a715b45653fa2986ecd35c82a20aeb0b74f5
Mikka138 <mickael.almeida1308@gmail.com>
2020-05-31 15:04:40 +0200

    small corrections

--- src/project/MainClient.java
+++ src/project/MainClient.java
@@ -4,6 +4,5 @@ public class MainClient {
     public static void main(String[] args) {
         new Client("localhost");
         Secure secure = new Secure();
-        System.out.println("password : " + secure.hash("password"));
     }
 }
